f1de00c6e957349369957a35eea09c9805e64931
MykhailoHrybchenko <mgrybchenko@gmail.com>
2022-10-26 23:46:39 +0300

    updated class Main + updated class Person

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main {
-}
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+    public class Main {
+
+        public static void main(String[] args) {
+            Person person = new Person("Igor", "Petrenko");
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String jsonPerson = gson.toJson(person);
+            System.out.println(jsonPerson);
+        }
+    }
+
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+
+    private String name;
+    private String lastname;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.lastname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }
